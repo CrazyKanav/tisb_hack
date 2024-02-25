9aa2dc0e394441f5aa79511d58cd0f2b5f8befd4
Kanav <81608989+CrazyKanav@users.noreply.github.com>
2024-02-25 11:59:03 +0530

    added sql

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///hack.db"
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(app, model_class=Base)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'Student {self.firstname}'
 
 @app.route('/')
 def hello():
-    return 'Preeti Meri'
+    return render_template("index.html")
