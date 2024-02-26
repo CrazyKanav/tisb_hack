fd080ab1ffc0ac36f40edda752d5dd33dbe0d7ea
Adiiiiiiiiiiiiiiiiii <113783917+Adiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-02-26 21:46:19 +0530

    signup

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(app, model_class=Base)
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     firstname = db.Column(db.String, nullable=False)
     lastname = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer, nullable=False)
@@ -33,22 +33,58 @@ class User(db.Model):
 
 class Teacher(db.Model):
     __tablename__ = 'teacher'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True)
     bio = db.Column(db.String, nullable=False)
     qualifications = db.Column(db.Text, nullable=False)
 
     user = relationship("User", back_populates="teacher")
 
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-    
+def check_teacher(user):
+  if user.teacher:
+    return True
+  else:
+    return False
 
-# session["name"] = None
+@app.route('/signup_teacher', methods=["GET", "POST"])
+def signup_teacher():
+    if request.method == "POST":
+        # Check if the user is logged in
+        if session["name"] == None:
+            return render_template("failure.html", msg="You need to log in to become a teacher")
+
+        # Retrieve additional data for teacher profile
+        bio = request.form.get("bio")
+        qualifications = request.form.get("qualifications")
+        
+        # Fetch the logged-in user
+        user_name = session['name']
+        user = User.query.filter_by(firstname=user_name).first()
+        print(user)
+        
+        # Create a new teacher profile associated with the logged-in user
+        new_teacher = Teacher(user=user, bio=bio, qualifications=qualifications)
+        db.session.add(new_teacher)
+        db.session.commit()
+
+        # Set a cookie indicating that the user is a teacher
+        session['is_teacher'] = True
+
+        return redirect(url_for("index"))
+    else:
+        if session['is_teacher'] == True:
+          return render_template("failure.html" , msg="Already a teacher", send_to_home=True)
+        if session['name'] == None:
+          return render_template("failure.html", msg="You need to log in to become a teacher", send_to_home=True)
+        return render_template("signup_teacher.html")
 
 @app.route('/')
 def index():
-    return render_template("index.html", name=session.get("name"))
+  # try:
+    print(session["name"])
+    user = User.query.filter_by(firstname=session["name"]).first()
+    # return render_template("index.html", name=user, teacher=session.get("is_teacher"))
+    return render_template("index.html", user=user, teacher=session.get("is_teacher"))
 
 @app.route('/register', methods=["POST"])
 def register():
@@ -92,7 +128,8 @@ def login():
       return render_template("failure.html", msg="password wrong")
   else:
     try:
-      if session["name"]:
+      if session["name"] != None:
+          print(session["name"])
           return redirect(url_for("index"))
       else:
         return render_template("login.html")
@@ -102,6 +139,7 @@ def login():
 @app.route('/logout')
 def logout():
   session["name"] = None
+  session["is_teacher"] = None
   return redirect(url_for("index"))
 
 @app.route('/test')
